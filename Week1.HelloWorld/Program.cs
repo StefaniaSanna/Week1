cb15669d511acac8487634c27a2c79504de2980a
sanna <sanna@LAPTOP-76P7RVTK>
2021-10-04 15:53:05 +0200

    Aggiungere i file di progetto.

---
+++ Week1.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
